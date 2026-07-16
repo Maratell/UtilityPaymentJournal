8d7d4455a08b496797ccec13006b4e4d3378cd80
Марат Кисатов <kisatov@mail.ru>
2026-07-16 15:08:42 +0300

    Добавлены логи для сервиса и контроллера доски жалоб

--- UtilityPaymentJournal/Interface/Service/IComplaintService.cs
+++ UtilityPaymentJournal/Interface/Service/IComplaintService.cs
@@ -5,8 +5,8 @@ namespace UtilityPaymentJournal.Interface.Service
     public interface IComplaintService
     {
         Task<ComplaintDto> CreateAsync(CreateComplaintDto createDto, CancellationToken cancellationToken = default);
-        Task<ComplaintDto?> GetByIdAsync(long id, CancellationToken cancellationToken = default);
-        Task<ComplaintDto?> EditAsync(long id, EditComplaintDto editDto, CancellationToken cancellationToken = default);
+        Task<ComplaintDto> GetByIdAsync(long id, CancellationToken cancellationToken = default);
+        Task<ComplaintDto> EditAsync(long id, EditComplaintDto editDto, CancellationToken cancellationToken = default);
         Task<bool> DeleteAsync(long id, CancellationToken cancellationToken = default);
         Task<IReadOnlyCollection<ComplaintDto>> GetAllAsync(CancellationToken cancellationToken = default);
     }
