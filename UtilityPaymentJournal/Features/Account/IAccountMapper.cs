fcda8523209247fb198dbd2137007bbf32d79522
Марат Кисатов <kisatov@mail.ru>
2026-07-22 13:00:11 +0300

    Контроллер и сервис аутентификации перенесен в папку Features

---
+++ UtilityPaymentJournal/Features/Account/IAccountMapper.cs
@@ -0,0 +1,11 @@
+﻿using UtilityPaymentJournal.Common.Enumerations;
+using UtilityPaymentJournal.Models.Authentication;
+
+namespace UtilityPaymentJournal.Features.Account
+{
+    public interface IAccountMapper
+    {
+        SignInDto ToSignInDto(SignInRequestViewModel signInRequestViewModel);
+        AuthenticationResultViewModel ToViewModel(AuthenticationResultDto authenticationResultDto);
+    }
+}
